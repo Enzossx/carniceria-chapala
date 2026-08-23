dd26798edbcd2bbae5bd56160f51509cd3a99017
Enzossx <enzosalvatierra44qgmail.com>
2026-08-23 17:08:56 -0300

    footer funcional

--- chapala_bd.sql
+++ chapala_bd.sql
@@ -7,3 +7,18 @@ CREATE TABLE IF NOT EXISTS cortes (
     precio DECIMAL(10,2) NOT NULL,
     imagen LONGBLOB
 );
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+select * from usuarios;
+
+select * from cortes;
+
+ALTER TABLE usuarios ADD COLUMN es_admin TINYINT(1) DEFAULT 0;
+
+UPDATE usuarios SET es_admin = 1 WHERE email = 'enzosalvatierra44@gmail.com';
